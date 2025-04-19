f1fc1e21e6d992e34c6bb246da867b0728c53afe
AnjelToppo <190910237+AnjelToppo@users.noreply.github.com>
2025-04-19 17:13:35 +0530

    Add People Error Feature

--- tip-calculator-app-main/js/main.js
+++ tip-calculator-app-main/js/main.js
@@ -8,44 +8,17 @@ const customSelectedTip = document.querySelector('#selected-tip input[type=numbe
 const tipAmountEl = document.getElementsByClassName('tip__amount')[0];
 const totalAmountEl = document.getElementsByClassName('total__amount')[0];
 
+const billAmount = document.getElementById('bill-amount');
+const peopleCount = document.getElementById('people-count');
+
+const peopleErrorEl = document.getElementsByClassName('people-error')[0];
 
 function activateReset() {
     resetBtnEl.disabled = false;
 }
 
-const billAmount = document.getElementById('bill-amount');
-billAmount.addEventListener('keyup', (e) => {
-    activateReset();
-    displayResult();
-})
-
-const peopleCount = document.getElementById('people-count');
-peopleCount.addEventListener('keyup', (e) => {
-    activateReset();
-    displayResult();
-
-})
-
-form.addEventListener('reset', (e) => {
-    selectedTip.dataset.selectedTip = '';
-    totalAmountEl.textContent = '$0.00';
-    tipAmountEl.textContent = '$0.00';
-    resetBtnEl.disabled = true;
-})
-
-setSelectedTip.forEach(input => {
-    input.addEventListener('click', (e) => {
-        customSelectedTip.value = '';
-        selectedTip.dataset.selectedTip = e.target.value.includes('%') ?  e.target.value : '';
-        activateReset();
-        displayResult();
-    })
-
-})
-
 function displayResult() {
     let tipPercentage = selectedTip.dataset.selectedTip?.includes('%') ?  selectedTip.dataset.selectedTip.replace('%', '') / 100 : '';
-    console.log(tipPercentage)
     if (billAmount.value.trim() === '' || +billAmount.value === 0 || selectedTip.dataset.selectedTip.length <= 1 || peopleCount.value === '' || +peopleCount.value === 0 ) {
         totalAmountEl.textContent = '$0.00';
         tipAmountEl.textContent = '$0.00';
@@ -61,12 +34,51 @@ function displayResult() {
         }
 
         let totalAmount = (+amt * +peopleCount.value + +billAmount.value) / +peopleCount.value;
-        console.log(totalAmount, totalAmount.toFixed(2))
         totalAmountEl.textContent = `$${totalAmount.toFixed(2)}`;
     }
-
 }
+
+function showPeopleError(count) {
+    if (count === 0) peopleErrorEl.innerText = "Can't be 0"
+}
+
+
+billAmount.addEventListener('keyup', (e) => {
+    activateReset();
+    displayResult();
+})
+
+setSelectedTip.forEach(input => {
+    input.addEventListener('click', (e) => {
+        customSelectedTip.value = '';
+        selectedTip.dataset.selectedTip = e.target.value.includes('%') ?  e.target.value : '';
+        activateReset();
+        displayResult();
+    })
+})
+
 customSelectedTip.addEventListener('keyup', (e) => {
     selectedTip.dataset.selectedTip = `${e.target.value}%`;
     displayResult();
 })
+
+function hidePeopleError(e) {
+    if (+e.target.value !== 0 || e.target.value.trim() === '') peopleErrorEl.innerText = "";
+}
+
+peopleCount.addEventListener('keyup', (e) => {
+    activateReset();
+    console.log(e.target.value);
+    showPeopleError(+e.target.value);
+    hidePeopleError(e);
+    displayResult();
+})
+
+form.addEventListener('reset', (e) => {
+    selectedTip.dataset.selectedTip = '';
+    totalAmountEl.textContent = '$0.00';
+    tipAmountEl.textContent = '$0.00';
+    resetBtnEl.disabled = true;
+})
+
+
