78d3dce50673bd0d85d3a4004c9819d0bd650c09
AnjelToppo <190910237+AnjelToppo@users.noreply.github.com>
2025-04-22 16:52:17 +0530

    Implement disabling generate button

--- password-generator-app/src/components/Options.jsx
+++ password-generator-app/src/components/Options.jsx
@@ -1,14 +1,19 @@
 import CharacterLength from "./CharacterLength.jsx";
 import CheckboxOptions from "./CheckboxOptions.jsx";
 import Strength from "./Strength.jsx";
+import {useContext} from "react";
+import {PasswordContext} from "../context/PasswordContext.jsx";
 
 export default function Options() {
+    const state = useContext(PasswordContext);
+    console.log(state.strengthLevel);
+
     return (
         <div className="options">
             <CharacterLength />
             <CheckboxOptions />
             <Strength />
-            <button className="generate-btn" type="button"><span className="generate-btn__text">Generate</span><span className="generate-btn__icon"></span></button>
+            <button className="generate-btn" type="button" disabled={state.strengthLevel === ''}><span className="generate-btn__text">Generate</span><span className="generate-btn__icon"></span></button>
         </div>
     )
 }
