81235de52e8aea876c31dc59d360814b00eb909a
AnjelToppo <190910237+AnjelToppo@users.noreply.github.com>
2025-04-23 14:32:48 +0530

    Add global reset styles

---
+++ frontend-quiz-app/src/App.jsx
@@ -0,0 +1,45 @@
+import GlobalStyles from "./styles/GlobalStyles.js";
+
+function App() {
+
+    return (<>
+            <GlobalStyles />
+            <div>
+                {/*Quiz menu start*/}
+
+                Welcome to the Frontend Quiz!
+                Pick a subject to get started.
+
+                HTML
+                CSS
+                JavaScript
+                Accessibility
+
+                {/*Quiz menu end*/}
+
+                {/*Quiz question start*/}
+
+                Question {/*number*/} of 10
+
+                A
+                B
+                C
+                D
+
+                Submit answer
+
+                {/*Quiz question end*/}
+
+                {/*Quiz completed start*/}
+
+                Quiz completed
+                You scored...
+
+                {/*score*/} out of 10
+
+                {/*Quiz completed end*/}
+            </div>
+        </>)
+}
+
+export default App
---
+++ frontend-quiz-app/src/styles/GlobalStyles.js
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    :root {
+        --white: #FFF;
+        --black: #000;
+        --grey-500: #626C7F;
+        --grey-50: #F4F6FA;
+        --blue-950: #2D3949;
+        --blue-900: #313E51;
+        --blue-850: #3B4D66;
+        --blue-500: #306AFF;
+        --blue-300: #ABC1E1;
+        --blue-100: #EDF1F9;
+        --blue-50: #EBF0FF;
+        --green-500: #2FD887;
+        --green-100: #E0FDEF;
+        --purple-600: #A729F5;
+        --purple-100: #F6E7FF;
+        --orange-400: #FF7E35;
+        --orange-50: #FFF5ED;
+        --red-500: #EE5454;
+        
+        --spacing-100: 0.8rem;
+        --spacing-200: 1.6rem;
+        --spacing-300: 2.4rem;
+        --spacing-400: 3.2rem;
+        --spacing-500: 4rem;
+        --spacing-600: 4.8rem;
+        --spacing-800: 6.4rem;
+        --spacing-1600: 12.8rem;
+        --spacing-1800: 14.4rem;
+        --spacing-2300: 18.4rem;
+    }
+    
+    * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+    
+    html {
+        font-size: 62.5%;
+    }
+    
+    button {
+        cursor: pointer;
+    }
+`;
+export default GlobalStyles;
