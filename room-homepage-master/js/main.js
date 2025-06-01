e72d6f9f5613995d34b71ce1470ff2b6534fff5c
AnjelToppo <190910237+AnjelToppo@users.noreply.github.com>
2025-06-01 20:20:21 +0530

    Finish Room homepage

--- room-homepage-master/js/main.js
+++ room-homepage-master/js/main.js
@@ -4,6 +4,9 @@ const allNextBtnEl = document.querySelectorAll('[aria-label="next"]');
 const allHeroImages = document.querySelectorAll('.hero__image');
 const allHeroContents = document.querySelectorAll('.hero__content');
 
+const menuBtnEl = document.querySelector("button[aria-label='menu']");
+const closeMenuBtnEl = document.querySelector(".close-menu");
+
 let currentTab = 0;
 
 allPrevBtnEl.forEach(prevBtnEl => {
@@ -40,4 +43,12 @@ allNextBtnEl.forEach(nextBtnEl => {
         })
         allHeroContents[currentTab].removeAttribute('hidden');
     })
+})
+
+menuBtnEl.addEventListener('click', (e) => {
+    menuBtnEl.setAttribute('aria-expanded', 'true');
+})
+
+closeMenuBtnEl.addEventListener('click', (e) => {
+    menuBtnEl.setAttribute('aria-expanded', 'false');
 })
